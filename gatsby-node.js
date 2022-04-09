9c5fe95292a8ceda73b1abaffd9a8016c5c99bc6
Chang-ji <75286090+Chang-ji@users.noreply.github.com>
2022-04-09 14:13:05 +0900

    Slug에 대해 알아보고, 데이터 추가해보기

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,6 +16,7 @@ exports.createPages = async ({ actions }) => {
 // You can delete this file if you're not using it
 
 const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -31,4 +32,15 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       },
     },
   });
+};
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode });
+
+    createNodeField({ node, name: 'slug', value: slug });
+  }
 };
